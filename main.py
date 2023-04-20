10805c47d1b15311182f2b8c7ebaacd7add3d5a2
otherpeopIe <mackenzierirving@gmail.com>
2023-04-19 22:16:41 -0400

    Learned functions and input in Python.

---
+++ main.py
@@ -0,0 +1,28 @@
+print("Hello World!")
+
+
+def HelloWorld(): # (Data Types below)
+    msg = "Hello World" # (String) (Any text, symbol, unicode character, UTF, ASCII, etc.) 
+    # myNum = 5 (Numbers)
+    # myFloat = 5.5 (Decimals)
+    # myBool = True (True or False)
+
+    print("Hello World!")
+
+
+def AddTwoNumbers(a, b): 
+    return a + b
+
+def Start():
+    print("Welcome to my game!")
+    print("What is your name?")
+    name = input()
+    print("Hello " + name + "!")
+
+
+
+# in most other languages you have to have an entry function to start your program or none of the functions will run - functions only run when you call them, so if you never tell the OS which function you start with, it doesn't know, so in most languages they're like we're telling you here the function is starting at main
+# entry point
+Start()
+
+
